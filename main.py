cd9cea7b6c2cd0d654261a9100c367cd7c5b0df7
Hyeon-Jun <hcha2@binghamton.edu>
2024-12-04 18:21:35 -0500

    final

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import pygame
-#import your controller
+from src.controller import Controller  # Import the controller
 
 def main():
     pygame.init()
-    #Create an instance on your controller object
-    #Call your mainloop
-    
-    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
+    game = Controller()  # Create an instance of the Controller object
+    game.run()  # Call the mainloop
 
 # https://codefather.tech/blog/if-name-main-python/
 if __name__ == '__main__':
